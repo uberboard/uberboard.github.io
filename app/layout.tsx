1baeae3abaf2ea4dfef757044c26d97ea5f7d11d
Steffen Jacobs <steffen.jacobs.ext@micromerce.com>
2023-05-08 10:14:29 +0200

    disabled zooming on mobile devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,12 @@ import '../styles/globals.css';
 export const metadata: Metadata = {
     title: 'uberboard.io',
     description: 'Welcome to uberboard.io',
-    viewport: "width=device-width, initial-scale=1"
+    viewport: "width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: {
+   children,
+}: {
     children: React.ReactNode,
 }) {
     return (

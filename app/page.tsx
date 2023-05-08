9dad0b22c6a87b4c7009d38b54a222909bfa6fcc
Steffen Jacobs <steffen.jacobs.ext@micromerce.com>
2023-05-08 12:53:58 +0200

    fixed jumping animation

--- app/page.tsx
+++ app/page.tsx
@@ -9,91 +9,91 @@ export default function Home() {
     return (
         <>
             <div className={[styles.backgroundImage].join(" ")}>
-                <ScrollParallax strength={0.98} lerpEase={.95} isAbsolutelyPositioned={true}>
+                <ScrollParallax strength={0.98} lerpEase={1} isAbsolutelyPositioned={true}>
                     <span style={{backgroundImage: "url('/background/ocean/SVG/layer4.svg')"}}
-                         className={styles.backgroundLayer}/>
+                          className={styles.backgroundLayer}/>
                 </ScrollParallax>
-                <ScrollParallax strength={.9} lerpEase={.95} isAbsolutelyPositioned={true}>
+                <ScrollParallax strength={.9} lerpEase={1} isAbsolutelyPositioned={true}>
                     <span style={{backgroundImage: "url('/background/ocean/SVG/layer3.svg')"}}
-                         className={styles.backgroundLayer}/>
+                          className={styles.backgroundLayer}/>
                 </ScrollParallax>
-                <ScrollParallax strength={.7} lerpEase={.95} isAbsolutelyPositioned={true}>
+                <ScrollParallax strength={.7} lerpEase={1} isAbsolutelyPositioned={true}>
                     <span style={{backgroundImage: "url('/background/ocean/SVG/layer2.svg')"}}
-                         className={styles.backgroundLayer}/>
+                          className={styles.backgroundLayer}/>
                 </ScrollParallax>
-                <ScrollParallax strength={1.05} lerpEase={.95} isAbsolutelyPositioned={false}>
-                    <div className={styles.titleContainer}>
-                        <div className={styles.title}>uberboard.io</div>
+                <ScrollParallax strength={1.05} lerpEase={1} isAbsolutelyPositioned={false}>
+                    <span className={styles.titleContainer}>
+                        <span className={styles.title}>uberboard.io</span>
                         <div className={styles.subtitle}>creating dashboards with style</div>
-                    </div>
+                    </span>
                 </ScrollParallax>
-                <ScrollParallax strength={.4} lerpEase={.95} isAbsolutelyPositioned={true}>
+                <ScrollParallax strength={.4} lerpEase={1} isAbsolutelyPositioned={true}>
                     <span style={{backgroundImage: "url('/background/ocean/SVG/layer1.svg')"}}
-                         className={styles.backgroundLayer}/>
+                          className={styles.backgroundLayer}/>
                 </ScrollParallax>
-                <ScrollParallax strength={0} lerpEase={.95} isAbsolutelyPositioned={true}>
+                <ScrollParallax strength={0} lerpEase={1} isAbsolutelyPositioned={true}>
                     <span style={{backgroundImage: "url('/background/ocean/SVG/layer0.svg')"}}
-                         className={styles.backgroundLayer}/>
+                          className={styles.backgroundLayer}/>
                 </ScrollParallax>
             </div>
-        <div className={styles.container}>
-            <main className={styles.main}>
+            <div className={styles.container}>
+                <main className={styles.main}>
 
-                <div className={styles.center}>
-                    <div className={styles.description}>
-                        <p>
-                            <code className={styles.code}>curl -o-
-                                https://raw.githubusercontent.com/uberboard/uberboard-cli/main/install/install.sh |
-                                bash</code>
-                        </p>
+                    <div className={styles.center}>
+                        <div className={styles.description}>
+                            <p>
+                                <code className={styles.code}>curl -o-
+                                    https://raw.githubusercontent.com/uberboard/uberboard-cli/main/install/install.sh |
+                                    bash</code>
+                            </p>
+                        </div>
+
                     </div>
+                    <div className={styles.center}>
+                        <div className={styles.grid}>
+                            <a
+                                href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                                className={styles.card}
+                            >
+                                <h2 className={inter.className}>
+                                    Docs <span>-&gt;</span>
+                                </h2>
+                                <p className={inter.className}>
+                                    Find in-depth information about uberboard features and&nbsp;API.
+                                </p>
+                            </a>
 
-                </div>
-                <div className={styles.center}>
-                    <div className={styles.grid}>
-                        <a
-                            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                            className={styles.card}
-                        >
-                            <h2 className={inter.className}>
-                                Docs <span>-&gt;</span>
-                            </h2>
-                            <p className={inter.className}>
-                                Find in-depth information about uberboard features and&nbsp;API.
-                            </p>
-                        </a>
+                            <a
+                                href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                                className={styles.card}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <h2 className={inter.className}>
+                                    Learn <span>-&gt;</span>
+                                </h2>
+                                <p className={inter.className}>
+                                    Learn about Next.js in an interactive course with&nbsp;quizzes!
+                                </p>
+                            </a>
 
-                        <a
-                            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                            className={styles.card}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <h2 className={inter.className}>
-                                Learn <span>-&gt;</span>
-                            </h2>
-                            <p className={inter.className}>
-                                Learn about Next.js in an interactive course with&nbsp;quizzes!
-                            </p>
-                        </a>
-
-                        <a
-                            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                            className={styles.card}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <h2 className={inter.className}>
-                                Try <span>-&gt;</span>
-                            </h2>
-                            <p className={inter.className}>
-                                Instantly use your uberboard dashboard in the cloud.
-                            </p>
-                        </a>
+                            <a
+                                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                                className={styles.card}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <h2 className={inter.className}>
+                                    Try <span>-&gt;</span>
+                                </h2>
+                                <p className={inter.className}>
+                                    Instantly use your uberboard dashboard in the cloud.
+                                </p>
+                            </a>
+                        </div>
                     </div>
-                </div>
-            </main>
-        </div>
+                </main>
+            </div>
         </>
     )
 }
